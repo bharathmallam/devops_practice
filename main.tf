1decce9ed04ef0c1987dcec29127c76a8db993e5
Bharath mallam <bharathmallam@Bharaths-MacBook-Air.local>
2026-08-01 12:04:14 +0530

    add route table

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 resource "aws_vpc" "Test_vpc" {
   cidr_block = "10.0.0.0/16"
 }
+
+
+resource "aws_route_table" "Rt" {
+  vpc_id = aws_vpc.Test_vpc.id
+  tags = {
+    Name = "Main_Route_Table"
+  }
+}
+
+  

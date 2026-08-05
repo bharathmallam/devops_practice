7c84d84b272315b0b182ade59093d4705e1e8757
Bharath mallam <bharathmallam@Bharaths-MacBook-Air.local>
2026-08-05 11:08:15 +0530

    merge conflict

--- subnet.tf
+++ subnet.tf
@@ -1,4 +1,8 @@
 resource "aws_subnet" "name" {
+<<<<<<< HEAD
   cidr_block = "10.0.1.0/16"
+=======
+  cidr_block = "10.0.2.0/16"
+>>>>>>> dcb459f (tried reset command)
   vpc_id     = aws_vpc.Test_vpc.id
 }

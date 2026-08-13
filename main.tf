c27c11efbb240db2a0654d0a30b2971537ae394e
Bharath mallam <bharathmallam@Bharaths-MacBook-Air.local>
2026-08-13 15:06:59 +0530

    changing runner to main

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 resource "aws_vpc" "Test_vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "Development_VPC"
+    Name = "Terrafrom_VPC"
   }
 }
 
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.Test_vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.0.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 }

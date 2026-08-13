cfc1cb464919defa43c61f810962bd697aadfbe9
Bharath mallam <bharathmallam@Bharaths-MacBook-Air.local>
2026-08-13 12:40:09 +0530

    updated cidr block

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "Test_vpc" {
 
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.Test_vpc.id
-  cidr_block              = "10.0.0.0/24"
+  cidr_block              = "10.0.1.0/24"
   availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = true
 }

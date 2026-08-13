e40d0f77c651c27f463de0132c720940dc932c4d
Bharath mallam <bharathmallam@Bharaths-MacBook-Air.local>
2026-08-13 15:36:44 +0530

    added backend

--- backend.tf
+++ backend.tf
@@ -1,9 +1,7 @@
 terraform {
   backend "s3" {
-    bucket       = "devopsbucket08"                            # Name of your S3 bucket
-    key          = "environments/production/terraform.tfstate" # File path inside the bucket
-    region       = "ap-south-1"                                # AWS Region where bucket lives
-    encrypt      = true                                        # Encrypts the state file at rest
-    use_lockfile = true                                        # Native S3 state locking (Terraform 1.10+)
+    bucket = "devopsbucket08"                            # Name of your S3 bucket
+    key    = "environments/production/terraform.tfstate" # File path inside the bucket
+    region = "ap-south-1"                                # AWS Region where bucket lives
   }
 }
